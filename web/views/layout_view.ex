81fb70fdf3a7b8b464de8aeae0b3de01c499bb6a
Elijah Wilson <dev.tizz98@gmail.com>
2017-02-25 17:56:39 -0800

    active nav links

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,19 @@
 defmodule PineappleIo.LayoutView do
   use PineappleIo.Web, :view
+
+  def is_login_path?(conn, method) do
+    current_path = Path.join(["/" | conn.path_info])
+    cond do
+      session_path(conn, method) == current_path -> true
+      true -> false
+    end
+  end
+
+  def is_register_path?(conn, method) do
+    current_path = Path.join(["/" | conn.path_info])
+    cond do
+      registration_path(conn, method) == current_path -> true
+      true -> false
+    end
+  end
 end
